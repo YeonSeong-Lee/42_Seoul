e6afb84d9d3a2a980e494bd21ccbdbb2adf85390
YeonSeong-Lee <yeonseong.dev@gmail.com>
2022-06-04 21:56:56 +0900

    CHORE: norm

--- 2_circle/push_swap/main.c
+++ 2_circle/push_swap/main.c
@@ -6,13 +6,13 @@
 /*   By: seongyle <seongyle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 14:05:15 by seongyle          #+#    #+#             */
-/*   Updated: 2022/06/04 21:56:05 by seongyle         ###   ########seoul.kr  */
+/*   Updated: 2022/06/04 21:56:46 by seongyle         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int argc, char	**argv)
+int	main(int argc, char **argv)
 {
 	t_stack	stack_a;
 	t_stack	stack_b;
