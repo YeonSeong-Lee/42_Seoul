c54755a4a0e2c16c68194f6bbdd432c3a4036eda
YeonSeong-Lee <yeonseong.dev@gmail.com>
2022-06-11 20:55:49 +0900

    FEATURE: insertion sort need to optimize

--- 2_circle/push_swap/srcs/arrange/arrange_big_args.c
+++ 2_circle/push_swap/srcs/arrange/arrange_big_args.c
@@ -6,79 +6,49 @@
 /*   By: seongyle <seongyle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 21:49:06 by seongyle          #+#    #+#             */
-/*   Updated: 2022/06/09 20:49:50 by seongyle         ###   ########seoul.kr  */
+/*   Updated: 2022/06/11 20:48:23 by seongyle         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
-/*
-static	t_node	*get_large_pivot(t_stack *stack)
+
+static	int	get_smallest_index(t_stack *stack)
 {
 	t_node	*temp;
+	int		i;
 
-	temp = stack->top;
-	while (temp)
+	i = 0;
+	while (1)
 	{
-		if (temp->index == stack->size * 2 / 3)
-			return (temp);
-		temp = temp->next;
+		temp = stack->top;
+		while (temp)
+		{
+			if (temp->index == i)
+				return (i);
+			temp = temp->next;
+		}
+		i++;
 	}
-	return (NULL);
+	return (-1);
 }
 
-static	t_node	*get_small_pivot(t_stack *stack)
-{
-	t_node	*temp;
-
-	temp = stack->top;
-	while (temp)
-	{
-		if (temp->index == stack->size / 3)
-			return (temp);
-		temp = temp->next;
-	}
-	return (NULL);
-}
-
-static	void	move_a_to_b(t_stack *stack_a, t_stack *stack_b)
+static	void	move_smallest_a_to_b(t_stack *stack_a, t_stack *stack_b)
 {
 	int	i;
-	int	ra_times;
-	int	pb_times;
-	int	rb_times;
 
-	if (stack_a->size <= 5)
-		return (arrange_under_5_args(stack_a, stack_b));
-	i = -1;
-	ra_times = 0;
-	pb_times = 0;
-	rb_times = 0;
-	while (i++ <= stack_a->size)
+	i = get_smallest_index(stack_a);
+	while (stack_a->top && i != -1)
 	{
-		if (stack_a->top->index >= get_large_pivot(stack_a)->index)
-		{
-			rotate_a(stack_a, 1);
-			ra_times++; 
-		}
-		else
-		{
-			push_b(stack_b, stack_a, 1);
-			pb_times++;
-			if (stack_a->top->index >= get_small_pivot(stack_a)->index)
-			{
-				rotate_b(stack_b, 1);
-				rb_times++;
-			}
-		}
+		if (stack_a->top->index == i)
+			return (push_b(stack_b, stack_a, 1));
+		rotate_a(stack_a, 1);
 	}
-	i = -1;
-	while (i++ <= ra_times + rb_times)
-		rotate_reverse_both(stack_a, stack_b);
-	move_a_to_b(ra_times);
 }
-*/
 
 void	arrange_big_args(t_stack *stack_a, t_stack *stack_b)
 {
-	
+	while (stack_a->size > 0)
+		move_smallest_a_to_b(stack_a, stack_b);
+	while (stack_b->size > 0)
+		push_a(stack_a, stack_b, 1);
 }
