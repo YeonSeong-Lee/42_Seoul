b38c56191edc7948826ecf7190830be9bf3843d9
YeonSeong-Lee <yeonseong.dev@gmail.com>
2022-06-26 14:51:50 +0900

    FEATURE: is_arraged 함수 추가

--- 2_circle/push_swap/srcs/arrange/arrange_stack.c
+++ 2_circle/push_swap/srcs/arrange/arrange_stack.c
@@ -6,14 +6,33 @@
 /*   By: seongyle <seongyle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 20:24:32 by seongyle          #+#    #+#             */
-/*   Updated: 2022/06/09 19:57:22 by seongyle         ###   ########seoul.kr  */
+/*   Updated: 2022/06/26 14:51:11 by seongyle         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
+static int	is_arranged(t_stack *stack_a)
+{
+	size_t	i;
+	t_node	*temp;
+	
+	i = 0;
+	temp = stack_a->top;
+	while (temp)
+	{
+		if (i != temp->index)
+			return (0);
+		temp = temp->next;
+		i++;
+	}
+	return (1);
+}
+
 void	arrange_stack(t_stack *stack_a, t_stack *stack_b)
 {
+	if (is_arranged)
+		return ;
 	if (stack_a->size <= 5)
 		return (arrange_under_5_args(stack_a, stack_b));
 	arrange_big_args(stack_a, stack_b);
