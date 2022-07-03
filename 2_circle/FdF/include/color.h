efb7d572c351543a8801d69a72fbec338c097edb
YeonSeong-Lee <yeonseong.dev@gmail.com>
2022-07-03 15:31:08 +0900

    FEAT: add color

--- 2_circle/FdF/include/color.h
+++ 2_circle/FdF/include/color.h
@@ -6,7 +6,7 @@
 /*   By: seongyle <seongyle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 15:18:57 by seongyle          #+#    #+#             */
-/*   Updated: 2022/07/03 15:23:41 by seongyle         ###   ########seoul.kr  */
+/*   Updated: 2022/07/03 15:30:47 by seongyle         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@
 # define RED 0x00FF0000
 # define GREEN 0x0000FF00
 # define BLUE 0x000000FF
+# define WHITE 0x00FFFFFF
+# define BROWN 0x00A52A2A
 
 #endif
