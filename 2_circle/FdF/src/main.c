044ec8718ed2a60919b3859b71cc843b860129b2
YeonSeong-Lee <yeonseong.dev@gmail.com>
2022-07-10 22:06:38 +0900

    WIP: read map

--- 2_circle/FdF/src/main.c
+++ 2_circle/FdF/src/main.c
@@ -6,15 +6,21 @@
 /*   By: seongyle <seongyle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 21:32:02 by seongyle          #+#    #+#             */
-/*   Updated: 2022/07/10 21:10:52 by seongyle         ###   ########seoul.kr  */
+/*   Updated: 2022/07/10 22:01:16 by seongyle         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fdf.h"
-#include <stdio.h> // must delete
-#include <unistd.h> // must delete
+#include <stdio.h> //
 
 int	main(void)
 {
-	read_map();
+	char	**temp;
+	
+	temp = map_reader("./maps/42.fdf");
+	while (temp)
+	{
+		printf("%s", *temp);
+		temp++;
+	}
 }
--- 2_circle/FdF/src/paser/map_reader.c
+++ 2_circle/FdF/src/paser/map_reader.c
@@ -6,24 +6,40 @@
 /*   By: seongyle <seongyle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 01:54:38 by seongyle          #+#    #+#             */
-/*   Updated: 2022/07/10 21:24:57 by seongyle         ###   ########seoul.kr  */
+/*   Updated: 2022/07/10 22:06:11 by seongyle         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
 #include "../fdf.h"
+#include <stdio.h> //
 
-char	**read_map()
+static int	open_map(char *path)
 {
-	int	fd;
-	char	**temp;
+	int		fd;
 	
-	fd = open("./maps/42.fdf",  O_RDONLY);
+	fd = open(path, O_RDONLY);
 	if (fd < 0)
 	{
 		write(2, "error can not open\n", 18);
 		// error일 때 따로 처리해야함.
 	}
-	temp = ft_split(get_next_line(fd), ' ');
+	return (fd);
+}
+
+char	**map_reader(char *path)
+{
+	int 	fd;
+	char	*temp;
+	char	**map;
 	
+	map = (char**)malloc(1000000);
+	fd = open_map(path);
+	while (temp = get_next_line(fd))
+	{
+		*map = temp;
+		map++;
+	}
+	return (map);
+	//원래 할당되었던 map의 주소를 반환해야함.
 }
